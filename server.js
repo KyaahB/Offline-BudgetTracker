9ede72270b24c534653fd79249668e1ced4a4801
Kyaah <kyaahh@gmail.com>
2021-08-21 10:22:43 -0500

    changed database name

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+
+// connection change for deployment
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Progressive-App", {useNewUrlParser: true,useFindAndModify: false,useCreateIndex: true,unUnifiedTopology: true});
 
 // routes
 app.use(require("./routes/api.js"));
